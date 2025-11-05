585a45dc4545b5ff477a9ed812c870459e2ab5f4
Edwin Camino <edwinpcamino@gmail.com>
2025-11-05 08:35:47 -0500

    Creacion de migracion

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi blog</title>
+</head>
+<body>
+    <h1>Lista de artículos</h1>
+    <?php $articles = []; ?>
+    @foreach ($articles as $article)
+        <h3>{{ $article->description }}</h3>
+    @endforeach
+    
+</body>
+</html>
